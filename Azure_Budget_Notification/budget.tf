b039c970851cf9d270adebcb221f3f5dcde7d169
Digbijayinee Clara <digbijayinee.clara@stackguardian.io>
2024-07-08 19:06:18 +0530

    azure-cost-test

--- Azure_Budget_Notification/budget.tf
+++ Azure_Budget_Notification/budget.tf
@@ -1,7 +1,73 @@
-resource "azurerm_cost_anomaly_alert" "cost_notification" {
-  name            = "cost_notification"
-  display_name    = "cost_notification"
-  #subscription_id = "/subscriptions/00000000-0000-0000-0000-000000000000"
-  email_subject   = "My Test Anomaly Alert"
-  email_addresses = ["digbijayinee.clara@stackguardian.io","johannes.scheuerer@stackguardian.io"]
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_resource_group" "SG_RG" {
+  name     = "SG_RG"
+  location = "eastus"
+}
+
+resource "azurerm_monitor_action_group" "monitor_action" {
+  name                = "monitor_action"
+  resource_group_name = azurerm_resource_group.SG_RG.name
+  short_name          = "SG_RG"
+}
+
+resource "azurerm_consumption_budget_subscription" "budget" {
+  name            = "budget"
+  subscription_id = data.azurerm_subscription.current.id
+
+  amount     = 1000
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2022-06-01T00:00:00Z"
+    end_date   = "2022-07-01T00:00:00Z"
+  }
+
+  filter {
+    dimension {
+      name = "ResourceGroupName"
+      values = [
+        azurerm_resource_group.example.name,
+      ]
+    }
+
+    tag {
+      name = "foo"
+      values = [
+        "bar",
+        "baz",
+      ]
+    }
+  }
+
+  notification {
+    enabled   = true
+    threshold = 80
+    operator  = "EqualTo"
+
+    contact_emails = [
+      "foo@example.com",
+      "bar@example.com",
+    ]
+
+    contact_groups = [
+      azurerm_monitor_action_group.example.id,
+    ]
+
+    contact_roles = [
+      "Owner",
+    ]
+  }
+
+  notification {
+    enabled        = false
+    threshold      = 100.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "digbijayinee.clara@stackguardian.io",
+      "johannes.scheuerer@stackguardian.io",
+    ]
+  }
 }
