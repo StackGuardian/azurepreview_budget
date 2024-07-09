3ff7619cbbaaae2d53669ffdaf415513c4f447d5
Digbijayinee Clara <digbijayinee.clara@stackguardian.io>
2024-07-09 10:47:03 +0530

    new-commit

--- Azure_Budget_Notification/budget.tf
+++ Azure_Budget_Notification/budget.tf
@@ -19,7 +19,7 @@ resource "azurerm_consumption_budget_subscription" "budget" {
   time_grain = "Monthly"
 
   time_period {
-    start_date = "2024-06-01T00:00:00Z"
+    start_date = "2024-07-01T00:00:00Z"
     #end_date   = "2024-07-01T00:00:00Z"
   }
 
