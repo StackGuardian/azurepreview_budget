96f1ad425c5333cf592feb1b1b098f9dfcfe71df
Digbijayinee Clara <digbijayinee.clara@stackguardian.io>
2024-07-09 11:05:26 +0530

    new-commit

--- Azure_Budget_Notification/budget.tf
+++ Azure_Budget_Notification/budget.tf
@@ -1,67 +1,30 @@
 data "azurerm_subscription" "current" {}
 
-resource "azurerm_resource_group" "SG_RG" {
-  name     = "SG_RG"
-  location = "eastus"
-}
-
-resource "azurerm_monitor_action_group" "monitor_action" {
-  name                = "monitor_action"
-  resource_group_name = azurerm_resource_group.SG_RG.name
-  short_name          = "SG_RG"
-}
-
 resource "azurerm_consumption_budget_subscription" "budget" {
   name            = "budget"
   subscription_id = data.azurerm_subscription.current.id
 
-  amount     = 1000
+  amount     = 50
   time_grain = "Monthly"
 
   time_period {
     start_date = "2024-07-01T00:00:00Z"
-    #end_date   = "2024-07-01T00:00:00Z"
-  }
-
-  filter {
-    dimension {
-      name = "ResourceGroupName"
-      values = [
-        azurerm_resource_group.SG_RG.name,
-      ]
-    }
-
-    tag {
-      name = "foo"
-      values = [
-        "bar",
-        "baz",
-      ]
-    }
   }
 
   notification {
     enabled   = true
     threshold = 80
-    operator  = "EqualTo"
+    operator  = "GreaterThan"
 
     contact_emails = [
-      "foo@example.com",
-      "bar@example.com",
-    ]
-
-    contact_groups = [
-      azurerm_monitor_action_group.monitor_action.id,
-    ]
-
-    contact_roles = [
-      "Owner",
+      "digbijayinee.clara@stackguardian.io",
+      "johannes.scheuerer@stackguardian.io",
     ]
   }
 
   notification {
     enabled        = false
-    threshold      = 100.0
+    threshold      = 80
     operator       = "GreaterThan"
     threshold_type = "Forecasted"
 
