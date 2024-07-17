071249d475bac9cf5bc719f15ebf56a4758066f6
Johannes Scheuerer <joscheuerer@gmail.com>
2024-07-17 15:24:08 +0200

    Update variables.tf

--- GCP_Budget_Notification/variables.tf
+++ GCP_Budget_Notification/variables.tf
@@ -1,5 +1,10 @@
 variable "billing-account" {
   type = string
-  description = "GCP Billing Account
+  description = "GCP Billing Account"
+  default = ""
+}
+variable "email" {
+  type = string
+  description = "Email to notify"
   default = ""
 }
