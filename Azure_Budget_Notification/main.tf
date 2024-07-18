e06eba927d965f36534af9d5c35cf28af09a9927
Digbijayinee Clara <digbijayinee.clara@stackguardian.io>
2024-07-18 10:45:30 +0530

    azure-buget-changes

--- Azure_Budget_Notification/main.tf
+++ Azure_Budget_Notification/main.tf
@@ -5,16 +5,16 @@ resource "azurerm_consumption_budget_subscription" "budget" {
   subscription_id = data.azurerm_subscription.current.id
 
   amount     = var.amount
-  time_grain = "Monthly"
+  time_grain = var.time_grain
 
   time_period {
-    start_date = "2024-07-01T00:00:00Z"
+    start_date = var.start_date
   }
 
   notification {
     enabled   = true
     threshold = var.threshold
-    operator  = "GreaterThan"
+    operator  = var.operator
     threshold_type = var.threshold_type
 
     contact_emails = var.emails
--- Azure_Budget_Notification/variables.tf
+++ Azure_Budget_Notification/variables.tf
@@ -1,20 +1,53 @@
 variable "name" {
   type = string
+  description = "The name which should be used for this Subscription Consumption Budget."
   default = "SG_Azure_Budget_Notifications"
 }
 variable "amount" {
   type = number
+  description = "The total amount of cost to track with the budget."
   default = 20
 }
 variable "threshold" {
   type = number
+  description = "Threshold value associated with a notification. Notification is sent when the cost exceeded the threshold."
   default = 20
 }
 variable "threshold_type" {
   type = string
-  default = "Forecasted" # or "ACTUAL"
+  description = "The type of threshold for the notification. This determines whether the notification is triggered by forecasted costs or actual costs."
+  default = "Forecasted" # or "Actual"
 }
 variable "emails" {
   type = list
-  default = []
+  description = "Specifies a list of email addresses to send the budget notification to when the threshold is exceeded."
+  #default = []
+}
+variable "time_grain" {
+  type =  string
+  description = <<DESCRIPTION
+  The time covered by a budget. Tracking of the amount will be reset based on the time grain.Must be one of the following
+  - BillingAnnual
+  - BillingMonth
+  - BillingQuarter
+  - Annually 
+  - Monthly  
+  - Quarterly.
+  Defaults is set to Monthly.
+  DESCRIPTION
+}
+variable "start_date" {
+  type = string
+  description = "The start date for the budget. The start date must be first of the month and should be less than the end date. Budget start date must be on or after June 1, 2017."
+  default = "2024-07-01T00:00:00Z"
+}
+variable "operator" {
+  type = string
+  description = <<DESCRIPTION
+  The comparison operator for the notification. Must be one of the following:
+  - EqualTo
+  - GreaterThan
+  - GreaterThanOrEqualTo
+  DESCRIPTION
+  default = "GreaterThanOrEqualTo"
 }
