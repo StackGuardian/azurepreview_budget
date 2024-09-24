3ba945bec5b86f74aff3a88af9d0b98139607042
Johannes Scheuerer <joscheuerer@gmail.com>
2024-09-24 14:45:27 +0200

    Update budget.tf

--- Azure_Budget_Notification/budget.tf
+++ Azure_Budget_Notification/budget.tf
@@ -4,7 +4,7 @@ resource "azurerm_consumption_budget_subscription" "budget" {
   name            = var.name
   subscription_id = data.azurerm_subscription.current.id
 
-  amount     = var.total_limit
+  amount     = var.amount
   time_grain = var.time_grain
 
   time_period {
